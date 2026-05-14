be1a2f15c7a54c6631b5f40ebb5977cd2b686a31
aljci17 <alja.rajbar.pertoci@gmail.com>
2026-05-14 16:33:54 +0200

    Delujoča celotna koda(razen pri državah)

--- projektnaNaloga.py
+++ projektnaNaloga.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-wiki_url = "https://sl.wikipedia.org/wiki/Svetovni_pokal_v_smučarskih_skokih_2026"
+wiki_url = "https://sl.wikipedia.org/wiki/Svetovni_pokal_v_alpskem_smučanju"
 
 headers={"user-Agent": "Mozilla/5.0"} #preprečimo blokiranje wikipedije
 
@@ -14,3 +14,37 @@ if odgovor.status_code == 200:
             f.write(odgovor.text)
         print(f"Stran je shranjena v datoteko: {ime_datoteke}\n")
         juha=BeautifulSoup(odgovor.text,"html.parser")
+        tabela=juha.find("table", {"class":"wikitable"})
+        if tabela:
+            podatki=[]
+            vrstice=tabela.find_all("tr")
+            for vrstica in vrstice[1:]:
+                stolpci=vrstica.find_all(["td","th"])
+                if len(stolpci)>=4:
+                    try:
+                        ime=stolpci[0].text.strip()
+                        ime=ime.split("[")[0].strip() #odstranimo sklicevanja
+                        zlato=int(stolpci[1].text.strip())
+                        srebro=int(stolpci[2].text.strip())
+                        bron=int(stolpci[3].text.strip())
+                        podatki.append({
+                            "Država": ime,
+                            "zlato": zlato,
+                            "srebro": srebro,
+                            "bron": bron
+                        })
+                    except (ValueError,IndexError):
+                        continue
+            if podatki:
+                sorted_podatki = sorted(podatki, key=lambda x: (x["zlato"], x["srebro"], x["bron"]), reverse=True)
+                print(f"--- Urejeni podatki ---")
+                print(f"{'Država':<20} | {'zlato':<6} | {'srebro':<6} | {'bron':<6}")
+                print("-" *52)
+                for item in sorted_podatki:
+                    print(f"['Država':<20] | ['zlato':<6] | ['srebro':<6] | ['bron':<6]")
+            else:
+                print("V tabeli ni bilo mogoče najti številčnih podatkov.")
+        else:
+            print("Tabela na strani ni bila najdena")
+else:
+    print(f"Prišlo je do napake pri nalaganju strani. Statusna koda:{odgovor.status_code}")
