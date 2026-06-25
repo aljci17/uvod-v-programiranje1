55cb5e038d21a64257e09f8f845f14587cbe63e9
aljci17 <alja.rajbar.pertoci@gmail.com>
2026-06-25 11:46:00 +0200

    Skoraj dokončana analiza podatkov in popravljena csv dattoteka in gitignore

--- Projektna_naloga/izvoz_csv/csv_delo.py
+++ Projektna_naloga/izvoz_csv/csv_delo.py
@@ -3,11 +3,11 @@ import csv
 def shrani_csv_tekmovalci(ime_datoteke, podatki, drzave_tekmovalcev):
     with open(ime_datoteke, "w", encoding="utf-8", newline="") as f:
         w = csv.writer(f)
-        w.writerow(["Ime (ID)", "Zlato", "Srebro", "Bron", "Skupaj"])
+        w.writerow(["Ime", "DrzavaID", "Zlato", "Srebro", "Bron", "Skupaj"])
         for ime, m in sorted(podatki.items(), key=lambda x: x[1][3], reverse=True):
             drzava_id = drzave_tekmovalcev.get(ime, "")
-            ime_z_id=f"{ime} ({drzava_id})" if drzava_id else ime
-            w.writerow([ime_z_id, m[0], m[1], m[2], m[3]])
+            #ime_z_id=f"{ime} ({drzava_id})" if drzava_id else ime
+            w.writerow([ime, drzava_id, m[0], m[1], m[2], m[3]])
 
 
 def shrani_csv_drzave(ime_datoteke, drzave):
